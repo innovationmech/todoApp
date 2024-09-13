25acda1e433bd2a6bd53b60025940cf1b28c53f0
jackelyj <jackelyj@gmail.com>
2024-09-13 16:56:58 +0800

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by 李燕杰 on 2024/9/13.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
